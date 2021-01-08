63155583f9137ce1ba59da7356034583eac6d254
Natali Siécola <nataligrilobarros@gmail.com>
2021-01-08 11:21:59 -0300

    criando o menu e rodapé do layout da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LayoutModule } from './layout/layout.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuComponent, RodapeComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [MenuComponent, RodapeComponent]
+
 })
 export class LayoutModule { }
